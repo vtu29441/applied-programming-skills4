1c5c7faca52e9492de78cff556fcea07ed3f828f
vtu29441 <vtu29441@veltech.edu.in>
2026-09-15 14:51:51 +0530

    Time: 0 ms (100%), Space: 43.6 MB (68.97%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        dfs(root, 0, result);
+        return result;
+    }
+
+    private void dfs(TreeNode node, int level, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+
+        // The first node visited at a new level is the rightmost node
+        if (level == result.size()) {
+            result.add(node.val);
+        }
+
+        // Traverse right child first, then left child
+        dfs(node.right, level + 1, result);
+        dfs(node.left, level + 1, result);
+    }
+}
